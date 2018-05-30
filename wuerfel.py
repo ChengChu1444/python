7e6d5468c75c81695daf78c3a94ae6a8607f9734
ChengChu1444 <161wit14@o365.htl-leoben.at>
2018-05-30 05:57:47 -0700

    wuerfel.py updated

--- wuerfel.py
+++ wuerfel.py
@@ -2,3 +2,36 @@
 
 # 100 mal würfel simulieren
 # anzahl der 1er, 2er usw. ausgeben
+
+from random import *
+from array import *
+
+eins = 0
+zwei = 0
+drei = 0
+vier = 0
+fuenf = 0
+sechs = 0
+
+for i in range(0,100):
+    rng = randint(1,6)
+    if(rng == 1):
+        eins += 1
+    if(rng == 2):
+        zwei += 1
+    if(rng == 3):
+        drei += 1
+    if(rng == 4):
+        vier += 1
+    if(rng == 5):
+        fuenf += 1
+    if(rng == 6):
+        sechs += 1
+
+print("1er",eins)
+print("2er",zwei)
+print("3er",drei)
+print("4er",vier)
+print("5er",fuenf)
+print("6er",sechs)
+        
